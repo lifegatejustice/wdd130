69cf14f85e3f99e6a10723dd69c5bbff76ffa14c
lifegatejustice <jus24002@byui.edu>
2024-07-10 10:10:52 +0100

    wo5

---
+++ wwr/hhh.py
@@ -0,0 +1,40 @@
+alive = True
+
+def eat():
+    print("Eating...")
+
+def sleep():
+    print("Sleeping...")
+
+def code():
+    print("Coding...")
+
+def repeat():
+    print("Repeating...")
+
+
+
+
+
+    
+
+while alive:
+    eat()
+    sleep()
+    code()
+    repeat()
+
+    
+    
+    
+    alive = False
+
+# This is an example condition function, implement as needed
+def check_if_should_continue():
+    # Logic to determine if the loop should continue
+    # Return `True` to keep going, `False` to stop
+    # For now, let's just return `False` to stop after one iteration
+    return False
+
+    # comment: 
+# end while
